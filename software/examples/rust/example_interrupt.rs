586ab73064f488eeceeb0f0f33ec16b0f3f6027e
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:23:00 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_interrupt.rs
+++ software/examples/rust/example_interrupt.rs
@@ -1,32 +1,33 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{io16_v2_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{io16_v2_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your IO-16 Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your IO-16 Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let io16_v2_bricklet = IO16V2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let io = Io16V2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for input value events.
-    let input_value_listener = io16_v2_bricklet.get_input_value_receiver();
-    // Spawn thread to handle received events. This thread ends when the io16_v2_bricklet
+    // Create receiver for input value events.
+    let input_value_receiver = io.get_input_value_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `io` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in input_value_listener {
-            println!("Channel: {}", event.channel);
-            println!("Changed: {}", event.changed);
-            println!("Value: {}", event.value);
+        for input_value in input_value_receiver {
+            println!("Channel: {}", input_value.channel);
+            println!("Changed: {}", input_value.changed);
+            println!("Value: {}", input_value.value);
             println!();
         }
     });
 
-    // Set period for input value (channel 4) callback to 0.5s (500ms)
-    io16_v2_bricklet.set_input_value_callback_configuration(4, 500, false);
+    // Set period for input value (channel 4) callback to 0.5s (500ms).
+    io.set_input_value_callback_configuration(4, 500, false);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_output.rs
+++ software/examples/rust/example_output.rs
@@ -1,26 +1,26 @@
 use std::{error::Error, io, thread, time::Duration};
-use tinkerforge::{io16_v2_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{io16_v2_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your IO-16 Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your IO-16 Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let io16_v2_bricklet = IO16V2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let io = Io16V2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Configure channel 7 as output low
-    io16_v2_bricklet.set_configuration(7, 'o', false);
+    io.set_configuration(7, 'o', false);
 
     // Set channel 7 alternating high/low 10 times with 100 ms delay
     for i in 0..10 {
         thread::sleep(Duration::from_millis(100));
-        io16_v2_bricklet.set_selected_value(7, true);
+        io.set_selected_value(7, true);
         thread::sleep(Duration::from_millis(100));
-        io16_v2_bricklet.set_selected_value(7, false);
+        io.set_selected_value(7, false);
     }
 
     println!("Press enter to exit.");
