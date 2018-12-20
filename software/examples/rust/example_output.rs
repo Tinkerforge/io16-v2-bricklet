d1c4078735afa858f2f28bee9566dabc1134a165
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:55:13 +0100

    rust: Reformat examples.

--- software/examples/rust/example_output.rs
+++ software/examples/rust/example_output.rs
@@ -1,9 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use std::time::Duration;
-use tinkerforge::{ip_connection::IpConnection, 
-                  io16_v2_bricklet::*};
-
+use std::{error::Error, io, thread, time::Duration};
+use tinkerforge::{io16_v2_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -14,18 +10,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     let io = Io16V2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Configure channel 7 as output low
-		io.set_configuration(7, 'o', false);
+    // Configure channel 7 as output low
+    io.set_configuration(7, 'o', false);
 
-		// Set channel 7 alternating high/low 10 times with 100 ms delay
-		for i in 0..10{
-			thread::sleep(Duration::from_millis(100));
-			io.set_selected_value(7, true);
-			thread::sleep(Duration::from_millis(100));
-			io.set_selected_value(7, false);
-		}
+    // Set channel 7 alternating high/low 10 times with 100 ms delay
+    for i in 0..10 {
+        thread::sleep(Duration::from_millis(100));
+        io.set_selected_value(7, true);
+        thread::sleep(Duration::from_millis(100));
+        io.set_selected_value(7, false);
+    }
 
     println!("Press enter to exit.");
     let mut _input = String::new();
