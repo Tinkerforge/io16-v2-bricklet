f2c3f300b2e7222cf7fef11fe40cf96f64146432
Erik Fleckstein <erik@tinkerforge.com>
2020-08-19 16:51:52 +0200

    Add uc examples.

---
+++ software/examples/uc/example_interrupt.c
@@ -0,0 +1,44 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_io16_v2.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your IO-16 Bricklet 2.0
+
+void check(int rc, const char* msg);
+
+
+
+// Callback function for input value callback
+void input_value_handler(TF_IO16V2 *device, uint8_t channel, bool changed, bool value,
+                         void *user_data) {
+	(void)device; (void)user_data; // avoid unused parameter warning
+
+	tf_hal_printf("Channel: %u\n", channel);
+	tf_hal_printf("Changed: %s\n", changed ? "true" : "false");
+	tf_hal_printf("Value: %s\n", value ? "true" : "false");
+	tf_hal_printf("\n");
+}
+
+
+TF_IO16V2 io;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_io16_v2_create(&io, UID, hal), "create device object");
+
+
+	// Register input value callback to function input_value_handler
+	tf_io16_v2_register_input_value_callback(&io,
+	                                        input_value_handler,
+	                                        NULL);
+
+	// Set period for input value (channel 4) callback to 0.5s (500ms)
+	tf_io16_v2_set_input_value_callback_configuration(&io, 4, 500, false);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
---
+++ software/examples/uc/example_output.c
@@ -0,0 +1,37 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_io16_v2.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your IO-16 Bricklet 2.0
+
+void check(int rc, const char* msg);
+
+
+
+
+TF_IO16V2 io;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_io16_v2_create(&io, UID, hal), "create device object");
+
+
+	// Configure channel 7 as output low
+	check(tf_io16_v2_set_configuration(&io, 7, 'o', false), "call set_configuration");
+
+	// Set channel 7 alternating high/low 10 times with 100 ms delay
+	int i;
+	for(i = 0; i < 10; ++i) {
+		tf_hal_sleep_us(hal, 100 * 1000);
+		check(tf_io16_v2_set_selected_value(&io, 7, true), "call set_selected_value");
+		tf_hal_sleep_us(hal, 100 * 1000);
+		check(tf_io16_v2_set_selected_value(&io, 7, false), "call set_selected_value");
+	}
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
